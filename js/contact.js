8fc9d2dc4fc10fe10d57c16a509d015ba115b6fb
Musaab Abdalla <jamboosan209@gmail.com>
2023-09-29 21:30:18 +0200

    adding hotjar and configuring google maps

--- js/contact.js
+++ js/contact.js
@@ -43,23 +43,11 @@ function validateContactForm(event){
 
 
 
-
-
-
-
-
-
-
-
-
 function validateEmail(email){
   const regEx = /\S+@\S+\.\S+/;
   const patternMatches = regEx.test(email)
   return patternMatches;
 }
-
-
-
 
 
 function checkLength(value, len){
@@ -72,3 +60,31 @@ function checkLength(value, len){
 }
 
 contactForm.addEventListener("submit", validateContactForm)
+
+
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: 62.59091, lng: 6.44561 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 17,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "The Tech Blog ™",
+  });
+}
+
+initMap();
