25b75c401856d745156016dd160d3c51338fa8d7
Musaab Abdalla <jamboosan209@gmail.com>
2023-09-30 15:15:34 +0200

    still working with some bugs

--- js/ui/renderPost.js
+++ js/ui/renderPost.js
@@ -1,19 +1,19 @@
 
 const blogsContainer = document.querySelector(".articles")
 export function renderPosts(posts) {
-  posts.forEach(function (post) {
-    blogsContainer.innerHTML += `
+    posts.forEach(function (post) {
+        blogsContainer.innerHTML += `
             <div class="article-card">
                 <img src=${post._embedded["wp:featuredmedia"][0].source_url} alt="Article image" />
                 <div>
 
                     <h3>${post.title.rendered}</h3>
-                    <p id="active">September 3, 2023</p>
+                    <p id="active">${post.date}</p>
                     <div class="no-links">${post.excerpt.rendered}</div>
                     <a href="./blog-page.html?id=${post.id}" aria-label=""><span
                             class="link"></span></a>
                 </div>
             </div>
 `
-  })
+    })
 }
